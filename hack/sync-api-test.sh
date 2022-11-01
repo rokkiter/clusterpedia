7cff673323261062f3d337d2b807de359deeab29
rokkiter <101091030+rokkiter@users.noreply.github.com>
2022-11-01 17:09:15 +0800

    test22

--- hack/sync-api-test.sh
+++ hack/sync-api-test.sh
@@ -54,7 +54,7 @@ sync_create_tag(){
   else
     git add .
     git commit -m "tag:$CI_COMMIT_TAG sync api folder from ghippo repo"
-    git push API_REPO
+    git push $API_REPO
   fi
 
   check_tag
