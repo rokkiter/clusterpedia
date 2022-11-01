65e1ed0bdfdbbaac247e4f94d5f45bad3c4c6fbb
rokkiter <101091030+rokkiter@users.noreply.github.com>
2022-11-01 19:56:51 +0800

    test45

--- hack/sync-api-test.sh
+++ hack/sync-api-test.sh
@@ -4,33 +4,16 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-
-GITLAB_EMAIL="101091030+rokkiter@users.noreply.github.com"
-GITLAB_USER_NAME="rokkiter"
-GITLAB_USER_PASSWORD="Qq28605181"
-# todo token需要切换
 Token=${APIREPOTOKEN}
 CI_COMMIT_TAG="test"
-
-echo "~~~~~~~~~~~success do it"
 
 REPO_ROOT=$(git rev-parse --show-toplevel)
 API_ROOT="${REPO_ROOT}/staging/src/github.com/clusterpedia-io/api"
 API_REPO="https://$Token@github.com/rokkiter/api.git"
 
+# api仓库暂存地址
 TMP_DIR="/tmp/clusterpedia-api-$RANDOM"
-echo "------dir:$TMP_DIR"
 
-SOURCE_BRANCH="main"
-
-
-# init name && email config
-init_config(){
-  git config --global user.email "$GITLAB_EMAIL"
-  git config --global user.name "$GITLAB_USER_NAME"
-}
-
-# check tag, if exist, delete it
 check_tag(){
   if [ -n "$(git ls-remote --tags origin -l $CI_COMMIT_TAG)" ]; then
     echo "tag already exist, delete it before retag"
@@ -39,7 +22,6 @@ check_tag(){
   fi
 }
 
-# sync api repo then create tag
 sync_create_tag(){
   git clone $API_REPO $TMP_DIR
 
@@ -47,27 +29,23 @@ sync_create_tag(){
   cp -r $API_ROOT/* $TMP_DIR
   cd $TMP_DIR
 
-  echo "----create tag"
   if [ -n "$(git status | grep 'nothing to commit, working tree clean')" ]
   then
     echo "----api repo is not diff~"
   else
+    check_tag()
     git add .
     git commit -m "tag:$CI_COMMIT_TAG sync api folder from ghippo repo"
-    echo "----1"
     git push
-    echo "----2"
   fi
 
-#  check_tag
   git tag $CI_COMMIT_TAG -a -m "create tag"
-  git push
+  git push origin $CI_COMMIT_TAG
   echo "push tag success~"
 
   cd -
   rm -rf $TMP_DIR
 }
 
-init_config
 
 sync_create_tag
