95b1fa42c93328f91f1caf3e9ec4dcf726a601c2
rokkiter <101091030+rokkiter@users.noreply.github.com>
2022-11-04 09:33:21 +0800

    test10

--- hack/sync-api-test.sh
+++ hack/sync-api-test.sh
@@ -77,7 +77,7 @@ sync_api(){
       git filter-repo --subdirectory-filter staging/src/github.com/clusterpedia-io/api --force
       git remote add origin $API_REPO
       check_branch
-      git push origin $BRANCH_NAME  --allow-unrelated-histories
+      git push origin $BRANCH_NAME  -f
   fi
 
 
